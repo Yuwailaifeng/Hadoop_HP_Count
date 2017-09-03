ce4b83c65864dd43c5c67fbd2b564b26718518ad
Yuwailaifeng <Yuwailaifeng@users.noreply.github.com>
2017-09-04 02:17:26 +0800

    Update HPCount.sh

--- HPCount.sh
+++ HPCount.sh
@@ -1,4 +1,5 @@
 hadoop jar /usr/local/hadoop-2.8.0/share/hadoop/tools/lib/hadoop-streaming-2.8.0.jar \
 -file /home/hadoop/HPCount/mapper.py       -mapper /home/hadoop/HPCount/mapper.py \
 -file /home/hadoop/HPCount/reducer.py       -reducer /home/hadoop/HPCount/reducer.py \
--input /HPCount/DPAll_Color.txt  -output /HPCount/Output
+-input /HPCount/Input/*  -output /HPCount/Output
+
